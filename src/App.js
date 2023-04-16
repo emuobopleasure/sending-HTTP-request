39bbaa59771a45a6a5620c910b4565b720712f7a
Davis06 <davis06@outlook.com>
2023-04-16 20:04:36 +0100

    added try and catch to async function to manage error states

--- src/App.js
+++ src/App.js
@@ -5,27 +5,52 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([])
-  
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+
   const fetchMoviesHandler = async () => {
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json()
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          }
-        })
-        setMovies(transformedMovies)
-        // console.log(movies)
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
+      const data = await response.json()
+      // console.log(response)
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformedMovies)
+      setIsLoading(false)
+    } catch (error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
+
+    // console.log(movies)
   }
 
-  // useEffect(() => {
-  //   fetchMoviesHandler()
-  // }, [])
+  let content = <p>Found no movies</p>
 
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
 
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
 
   return (
     <React.Fragment>
@@ -33,7 +58,11 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && !error && movies.length === 0 && <p>Found no movies</p>}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && error && <p>{error}</p>}
+        {/* {content} */}
       </section>
     </React.Fragment>
   );
